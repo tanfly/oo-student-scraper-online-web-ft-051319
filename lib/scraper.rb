c9cfc1ed5e73ef9db26bb9e00b10acc370e24cc8
Stella McEwen <mcewen.stella@gmail.com>
2019-05-29 01:56:34 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,30 @@
+require 'nokogiri'
 require 'open-uri'
 require 'pry'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    students = []
+
+    page = Nokogiri::HTML(open(index_url))
+
+    page.css('div.student-card').each do |student|
+
+      students << {name: student.css('h4.student-name').text,
+      location: student.css('p.student-location').text,
+      profile_url: student.css('a').attribute('href').value}
+    end
+    students
   end
 
+
+
   def self.scrape_profile_page(profile_url)
-    
+    student_profile = {}
+
+    page = Nokogiri::HTML(open(profile_url))
+
   end
 
 end
-
