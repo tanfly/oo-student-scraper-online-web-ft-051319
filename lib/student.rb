c5b5362e4c4f1470ea9999b5f6eb622bcd92a69e
Stella McEwen <mcewen.stella@gmail.com>
2019-05-29 02:09:46 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -7,6 +7,8 @@ class Student
   def initialize(student_hash)
     student_hash.each {|k, v| self.send(("#{k}="), v)}
   end
+  @@all << self
+end
 
   def self.create_from_collection(students_array)
 
